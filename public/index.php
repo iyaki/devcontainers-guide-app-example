0b732647467180125739184120d0967df1e885c6
iyaki <me@iyaki.ar>
2025-03-02 18:35:36 -0300

    Even and odd greetings

--- public/index.php
+++ public/index.php
@@ -1,1 +1,16 @@
-Hola mundo
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$number = $_GET['number'] ?? null;
+$number = is_numeric($number) ? (int) $number : null;
+
+$helloed = match (true) {
+    ! is_int($number) => 'mundo',
+    is_even($number) => 'par',
+    default => 'impar',
+};
+
+echo "Hola, $helloed";
